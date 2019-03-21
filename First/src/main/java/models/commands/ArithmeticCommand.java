f429e4f85672a93307eafed84d967e054e144e72
vyciune <uzmuziu@gmail.com>
2019-03-21 21:19:52 +0200

    aritmetines komandos

--- First/src/main/java/models/commands/ArithmeticCommand.java
+++ First/src/main/java/models/commands/ArithmeticCommand.java
@@ -1,4 +1,57 @@
 package models.commands;
 
+import javafx.collections.ObservableList;
+import models.CPU;
+import models.WordFX;
+
 public class ArithmeticCommand {
+
+    private StackCommands stack;
+
+    public void ADD(CPU cpu, ObservableList<WordFX> memory) { //– sudeda du viršutinius steko elementus. Rezultatą padeda į stekoviršūnę ir steko rodyklę sumažina vienetu.ST[SP – 1] = ST [SP – 1] + ST [SP]; SP--;
+        int temp = cpu.SP(); //gaunu 1 reiksme
+        stack.POP(cpu); //sp padidinu
+        int ats = temp + cpu.SP();//sudedu reiksmes
+        stack.POP(cpu); //sp padidinu
+        stack.PUN(cpu, ats, memory );
+
+    }
+
+    public void SUB(CPU cpu, ObservableList<WordFX> memory) { //atima steko viršūnėje esantį elementą iš antro nuo viršaus elemento.Rezultatą padeda į steko viršūnę .ST [SP – 1] = ST [ SP – 1] - ST [SP]; SP--;
+        int temp = cpu.SP(); //gaunu 1 reiksme
+        stack.POP(cpu); //sp padidinu
+        int ats = cpu.SP() - temp;//atimu is 2 reiksmes pirma
+        stack.POP(cpu); //sp padidinu
+        stack.PUN(cpu, ats, memory );
+    }
+
+    public void MUL(CPU cpu, ObservableList<WordFX> memory) { //sudaugina du viršutinius steko elementus. Rezultatą padeda į stekoviršūnę ir steko rodyklę sumažina vienetu.ST [SP – 1] = ST [ SP – 1] * ST [SP]; SP--;
+        int temp = cpu.SP(); //gaunu 1 reiksme
+        stack.POP(cpu); //sp padidinu
+        int ats = cpu.SP() * temp;//atimu is 2 reiksmes pirma
+        stack.POP(cpu); //sp padidinu
+        stack.PUN(cpu, ats, memory );
+
+    }
+
+    public void DIV(CPU cpu, ObservableList<WordFX> memory) { //padalina antrą nuo viršaus steke esantį elementą iš viršūnėje esančio.Rezultatą padeda į steko viršūnę ir steko rodyklę sumažina vienetu.[SP – 1] =  [ SP – 1] / [SP]; SP--;
+        int temp = cpu.SP(); //gaunu 1 reiksme
+        stack.POP(cpu); //sp padidinu
+        int ats = cpu.SP() / temp;//atimu is 2 reiksmes pirma
+        stack.POP(cpu); //sp padidinu
+        stack.PUN(cpu, ats, memory );
+
+    }
+
+    // cia nesuprantu koki dari logini tipo kad !true = false ar bitu inversija??
+    //padariau inversija
+    public void NOT(CPU cpu, ObservableList<WordFX> memory) { //atlieka [SP] esančio žodžio loginį neigimą (inversiją).[SP] = !([SP])
+        int a = cpu.SP(); //gaunu 1 reiksme
+        int b = ~a; // this will reverts the bits 01000 which is 8 in decimal
+        WordFX fx = new WordFX(cpu.SP(), b);
+        memory.set(cpu.SP(), fx);
+
+    }
+
+
 }
--- First/src/main/java/models/commands/StackCommands.java
+++ First/src/main/java/models/commands/StackCommands.java
@@ -9,10 +9,11 @@ import models.WordFX;
 public class StackCommands {
 
     public void LD(int x, int y, CPU cpu, ObservableList<WordFX> memory) { //į steko viršūnę užkrauna reikšmę iš duomenų srities adresu 16 *x + y.  SP--; 0 < x,y < 16
-        WordFX temp = memory.get(16*x+y);
-        memory.set(CPU.SP(), temp);
         int sp = cpu.SP()-1;
         cpu.SP(sp);
+        WordFX temp = memory.get(16*x+y);
+        memory.set(CPU.SP(), temp);
+
 
     }
 
@@ -25,19 +26,22 @@ public class StackCommands {
     }
 
     public void PUN(CPU cpu, int value, ObservableList<WordFX> memory) { //– x kaip skaičių patalpina į steko viršūnę. SP++;[SP] = x.
+        int sp = cpu.SP()-1; //sumazinu sp kad rodytu i nauja tuscia langeli
+        cpu.SP(sp);
         String tmp = String.valueOf(value);//value i string
         WordFX wordfx = new WordFX(CPU.SP(), tmp); // sukuriu nauja wordFX
         memory.set(CPU.SP(), wordfx); // i SP vieta idedu nauja WORDFX(adresa nauja ty ta pati sp ir value jau string
-        int sp = cpu.SP()-1;
-        cpu.SP(sp);
+
     }
 
     // !!!!!!!!!!! cia nezinau kaip tai tiesiog string ikeliau
-    public void PUS(CPU cpu, String value, ObservableList<WordFX> memory) { //– x kaip simbolį patalpina į steko viršūnę. [SP] = x
-        WordFX wordfx = new WordFX(CPU.SP(), value); // sukuriu nauja wordFX
-        memory.set(CPU.SP(), wordfx); // i SP vieta idedu nauja WORDFX(adresa nauja ty ta pati sp ir value jau string
+    public void PUS(CPU cpu, String value, ObservableList<WordFX> memory) { //– x kaip simbolį patalpina į steko viršūnę. sp++;[SP] = x
         int sp = cpu.SP()-1;
         cpu.SP(sp);
+
+        WordFX wordfx = new WordFX(CPU.SP(), value); // sukuriu nauja wordFX
+        memory.set(CPU.SP(), wordfx); // i SP vieta idedu nauja WORDFX(adresa nauja ty ta pati sp ir value jau string
+
 
     }
 
